37464ca1ff58ecdbd18c5a99568dc13ccbadd68b
KoyamaKiryu <101534779+KoyamaKiryu@users.noreply.github.com>
2022-08-24 11:50:58 +0900

    Update request-inspect-app.py

--- ep6/request-inspect-app.py
+++ ep6/request-inspect-app.py
@@ -1,1 +1,7 @@
 from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE']
+
+@app.route
