7df10740049f0539d4f51b881dd2f9dde44fe09a
KoyamaKiryu <101534779+KoyamaKiryu@users.noreply.github.com>
2022-08-24 14:08:09 +0900

    Update request-inspect-app.py

--- ep6/request-inspect-app.py
+++ ep6/request-inspect-app.py
@@ -4,4 +4,23 @@ app = Flask(__name__)
 
 METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE']
 
-@app.route
+@app.route('/', defaults={'path': ''}, methods=METHODS)
+@app.route('/<path:path>', methods=METHODS)
+
+def index(path):
+	'''
+	リクエストのRequest-URI、メソッド及びヘッダーの内容をレスポンスする
+	'''
+	
+	return f'''
+	<h1>Path: /{path}</h1>
+	<h2>Method: {request.method}</h2>
+	<ul>
+		{'.join((f'<li>{name}: {value}</li>' for name, value in request.header))'}
+	</ul>
+	<h2>Request body</h2>
+	<p>{ request.get_data().decode() if request.get_date() else 'None.'}</p>
+	'''
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)
