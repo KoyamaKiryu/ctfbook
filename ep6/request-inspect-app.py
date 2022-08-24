259ec7af9a799990996745fccad0442ed5f308f7
KoyamaKiryu <101534779+KoyamaKiryu@users.noreply.github.com>
2022-08-24 14:17:44 +0900

    Update request-inspect-app.py

--- ep6/request-inspect-app.py
+++ ep6/request-inspect-app.py
@@ -6,21 +6,22 @@ METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE'
 
 @app.route('/', defaults={'path': ''}, methods=METHODS)
 @app.route('/<path:path>', methods=METHODS)
-
 def index(path):
-	'''
-	リクエストのRequest-URI、メソッド及びヘッダーの内容をレスポンスする
-	'''
-	
-	return f'''
-	<h1>Path: /{path}</h1>
-	<h2>Method: {request.method}</h2>
-	<ul>
-		{'.join((f'<li>{name}: {value}</li>' for name, value in request.header))'}
-	</ul>
-	<h2>Request body</h2>
-	<p>{ request.get_data().decode() if request.get_date() else 'None.'}</p>
-	'''
+  '''
+  リクエストのRequest-URI、メソッド及びヘッダーの内容をレスポンスする。
+  '''
+
+  return f'''
+  <h1>Path: /{path}</h1>
+  <h2>Method: {request.method}</h2>
+  <h2>Request headers</h2>
+  <ul>
+    {''.join((f'<li>{name}: {value}</li>' for name, value in request.headers))}
+  </ul>
+  <h2>Request body</h2>
+  <p>{ request.get_data().decode() if request.get_data() else 'None.' }</p>
+  '''
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000)
